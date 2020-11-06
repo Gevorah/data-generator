0ca01eae2bc187e11f44a4a6ecb0e2a00f890dcb
sebastianvilla17 <54720021+sebastianvilla17@users.noreply.github.com>
2020-11-06 12:11:53 -0500

    Test

---
+++ Test/datastructure/ABBTest.java
@@ -0,0 +1,14 @@
+package datastructure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ABBTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Test/datastructure/AVLTest.java
@@ -0,0 +1,14 @@
+package datastructure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AVLTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
